4f23879f9c9c410f906fd7de9a24485eab491809
Anders Astrom <anders@lyngon.com>
2023-06-30 15:21:09 +0000

    Bumped AWS provider version

--- infra/terraform.tf
+++ infra/terraform.tf
@@ -9,7 +9,7 @@ terraform {
     required_providers {
         aws = {
             source  = "hashicorp/aws"
-            version = "~> 3.28.0"
+            version = "~> 5.6.1"
         }
     }
     required_version = ">= 0.14.0"
