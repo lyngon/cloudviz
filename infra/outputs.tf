ba1f2570ffcb73bc48702486e6394e52138ef52f
Anders Astrom <anders@lyngon.com>
2023-06-30 16:03:01 +0000

    Remove useless output `account-name`

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,8 +1,3 @@
-output "account_name" {
-    description = "The target account that CloudViz will scan. Configure this for 'AWS Account name' under 'Settings' > 'Manage AWS Accounts' > 'Add AWS Accounts' > 'Step 4'"
-    value = data.aws_caller_identity.current.account_id
-}
-
 output "iam_role_arn" {
     description = "The ARN of the role that CloudViz will assume. Configure this for 'IAM Role ARN' under 'Settings' > 'Manage AWS Accounts' > 'Add AWS Accounts' > 'Step 4'"
     value = aws_iam_role.cloudviz.arn
